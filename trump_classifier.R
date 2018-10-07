43e407d63101b2761d03dfe22cf5151af21bfa11
Madison Volpe <mv1617@nyu.edu>
2018-10-07 09:51:55 -0400

    Changed picturelink feature

--- trump_classifier.R
+++ trump_classifier.R
@@ -1,6 +1,9 @@
+#Loading necessary packages 
 library(data.table)
 library(lubridate)
 library(dplyr)
+
+#Read in Data 
 trump <- as.data.frame(fread("trump_data.tsv", header = FALSE))
 names(trump) <- c("source", "time_posted", "text")
 
@@ -19,8 +22,8 @@ trump$quote <- ifelse(grepl('^"',trump$text), 1, 0) #if it begins with a quotati
 trump_filter <- trump%>%
   filter(quote==0)
 
-#Feature 3: Contains an Https(proxy for link)
-trump_filter$link <- ifelse(grepl("https",trump_filter$text), 1,0)
+#Feature 3: Contains a Link/ Picture 
+trump_filter$link <- ifelse(grepl("t.co",trump_filter$text), 1,0) #in the article he uses t.co 
 
 #Feature 4: Presence of a hastag 
 trump_filter$hashtag <- ifelse(grepl("#", trump_filter$text),1,0)
@@ -33,8 +36,4 @@ X <- left_join(trump,trump_filter)
 
 #relevel NAs as 0 
 
-#HI
 
-#Hello, I just learned how to use git! -Bill
-
-#I learned how to use git as well!- Ekanta
