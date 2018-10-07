53d816fcef5d191db96edcbcedf9be284785fa6b
Vergara-Spagnola <vergara-spagnola@Vergara-Spagnolas-MacBook-Pro.local>
2018-10-07 14:39:45 -0400

    Bill: Build a lot of models.  Added visualizations.  Edited mutuate NAs to 0s to prevent R from created three factors for binary variabls

--- trump_classifier.R
+++ trump_classifier.R
@@ -4,7 +4,8 @@ library(lubridate)
 library(dplyr)
 library(tidytext) #may have to download this if you do not have it 
 library(stringr)
-
+library(naivebayes)
+library(ggplot2)
 #Read in Data 
 trump <- as.data.frame(fread("trump_data.tsv", header = FALSE))
 names(trump) <- c("source", "time_posted", "text")
@@ -34,7 +35,7 @@ trump$time_posted <- ymd_hms(trump$time_posted)
   #Feature 4: Presence of a hastag 
   trump_filter$hashtag <- ifelse(grepl("#", trump_filter$text),1,0)
 
-  #Feature 5: Pesence of an exclamation point at the end of the tweet 
+  #Feature 5: Presence of an exclamation point at the end of the tweet 
   trump_filter$ExclamationEnd <- ifelse(grepl("!$", trump_filter$text),1,0)
 
   #Feature 6,7,8,9: Does the tweet contain negative emotion? Does the tweet contain positive emotion?
@@ -107,6 +108,17 @@ trump$time_posted <- ymd_hms(trump$time_posted)
                 PercentNeg = NegativeEmotions/sum(NegativeEmotions+PositiveEmotions),
                 PercentPos = PositiveEmotions/sum(PositiveEmotions+NegativeEmotions))
 
+    
+#Feature 10: Presence of capitalized words (Check if 3 consecutive letters are capitalized)
+    ##Omit if tweet contains link (because many links conta)
+    ## Should be at least 4 to omit News organizations such as ABC, WSJ, etc. 
+    trump_filter$capitalized <- ifelse(grepl("[A-Z]{4,}", trump_filter$text)== T &
+                                         grepl("t.co",trump_filter$text) ==F, 1, 0)
+     #Note sure how to exclude links with 4 consecutive letters 
+    trump_filter[(grepl("[A-Z]{4,}", trump_filter$text)== T &
+    grepl("t.co",trump_filter$text) ==F),] %>% select(source, text) %>% group_by(source) %>%
+      summarize(n = n())
+    
 #Final dataset 
     
     #note - that trump_filter only examined tweets written by trump and his staff
@@ -117,15 +129,20 @@ trump$time_posted <- ymd_hms(trump$time_posted)
 
     #relevel NAs as 0 
     
-    trump <- data.frame(apply(trump, 2, function(x){ ifelse(is.na(x), 0, x)}))
+    #trump <- as.data.frame(apply(trump, 2, function(x){ ifelse(is.na(x)==T, 0, x)}))
     
+    trump <-trump %>% mutate_all(function(x){ ifelse(is.na(x)==T, 0, x)})
+    'Note: I changed this because the previous code was creating 3 levels'
+    
+
     #check 
-    
     sum(is.na(trump))
 
 #clean work space#
     
     rm(nrc,Sentiments, SentimentsCount, tweet_words)
+    
+    
 
 ##### Part 2: Dividing Data into Training and Test Set ##### 
     
@@ -142,6 +159,210 @@ trump$time_posted <- ymd_hms(trump$time_posted)
     test <- trump %>% slice(split_size+1:n()) #splits size - we want it to be 20% 
     test_labels <- test$label
     test <- test %>% select(-label)
+
+    
+##### Part 4: Explore Data
+    
+ #Compare Number of Tweets by Hour
+trump %>%
+      group_by(hour, label) %>%
+      summarize(number_of_tweets = n()) %>%
+      ggplot() +
+      geom_line(aes(x=hour, y=number_of_tweets, group = label, color = label))
+
+#Compare Quotation Marks
+(quote_summary <- trump %>%
+      mutate(quote = ifelse(quote==1, "quote", "no quote")) %>%
+      count(label, quote))
+ggplot(quote_summary, aes(x=label, y = n, fill = quote)) +
+      geom_bar(stat = "identity", position = "dodge") +
+      labs(x = "", y = "Number of tweets", fill = "")
+
+#Compare Capitalization
+(capital_summary <- trump %>%
+        mutate(all_caps = ifelse(capitalized==1, "All Caps", "No All Caps")) %>%
+        count(label, all_caps))
+ggplot(capital_summary, aes(x=label, y=n, fill = all_caps))+
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "", y = "Number of tweets", fill = "")
+
+#Compare Links
+(links_summary <- trump %>%
+    mutate(link = ifelse(link==1, "Links", "No Link")) %>%
+    count(label, link))
+ggplot(links_summary, aes(x=label, y=n, fill = link))+
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "", y = "Number of tweets", fill = "")
+
+#Compare Hashtag
+(hashtag_summary <- trump %>%
+    mutate(hashtag = ifelse(hashtag==1, "Hashtag", "No Hashtag")) %>%
+      count(label, hashtag) )
+ggplot(hashtag_summary, aes(x=label, y=n, fill = hashtag))+
+         geom_bar(stat = "identity", position = "dodge") +
+         labs(x = "", y = "Number of tweets", fill = "")
+
+
+#Compare Exclamation Points 
+(exclaim_summary <- trump %>%
+    mutate(exclamation_point = ifelse(ExclamationEnd==1, "Exclamation Point", "No Exclamation Point")) %>%
+    count(label, exclamation_point) )
+ggplot(exclaim_summary, aes(x=label, y=n, fill = exclamation_point))+
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "", y = "Number of tweets", fill = "")
+
+
+
+
+#### Part 5: Naive Bayes ####
+#Function to Print Confusion Matrix and Calculate Accuracy 
+check_accuracy <- function(predictions, labels, phase){
+  confusion_matrix <- table(predictions, labels)
+  print(confusion_matrix)
+  accuracy <-  sum(diag(confusion_matrix )) / sum(confusion_matrix)
+  cat("Accuracy on", phase ,"Data: ", accuracy)
+}
+
+#Full Model
+#Create model and make predictions on training data
+nb_mod <- naive_bayes(x=select(train, -text, -time_posted ), y=train_labels)
+#Training Data
+pred_train_1 <- predict(nb_mod, train)
+check_accuracy(pred_train_1, train_labels, "Training")
+#Test
+pred_test_1 <- predict(nb_mod, test)
+check_accuracy(pred_test_1, test_labels, "Test")
+
+
+#Model 2: Drop All Caps Field 
+no_caps <- select(train, -text, -time_posted, -capitalized )
+nb_mod2 <- naive_bayes(x=no_caps, y=train_labels)
+#Training Data
+pred_train_2<- predict(nb_mod2, train)
+check_accuracy(pred_train_2, train_labels, "Training")
+#Test
+pred_test_2 <- predict(nb_mod2, test)
+check_accuracy(pred_test_2, test_labels, "Test")
+
+#Model 3: Drop Time 
+no_time <- select(train, -text, -time_posted, -capitalized, -hour)
+nb_mod3 <- naive_bayes(x=no_time, y=train_labels)
+nb_mod3$tables
+#Training Data
+pred_train_3 <- predict(nb_mod3, train)
+check_accuracy(pred_train_3, train_labels, "Training")
+#Test
+pred_test_3 <- predict(nb_mod3, test)
+check_accuracy(pred_test_3, test_labels, "Test")
+
+
+#Model 4: Drop  Links 
+no_links <- select(train, -text, -time_posted, -capitalized, -hour, -link)
+nb_mod4 <- naive_bayes(x=no_links, y=train_labels)
+#Training Data
+pred_train_4 <- predict(nb_mod4, train)
+check_accuracy(pred_train_4, train_labels, "Training")
+#Test
+pred_test_4 <- predict(nb_mod4, test)
+check_accuracy(pred_test_4, test_labels, "Test")
+'
+It looks like we should definitely keep links.
+'
+
+#Model 5: Drop  Positive Emotions
+no_positive <- select(train, -text, -time_posted, -capitalized, -hour
+                      -NumberPositiveEmotions, -Positive )
+nb_mod5 <- naive_bayes(x=no_positive, y=train_labels )
+#Training Data
+pred_train_5 <- predict(nb_mod5, train)
+check_accuracy(pred_train_5, train_labels, "Training")
+#Test Data
+pred_test_5 <- predict(nb_mod5, test)
+check_accuracy(pred_test_5, test_labels, "Training")
+
+
+#Model 6: Drop  Negative Emotions
+no_negative <- select(train, -text, -time_posted, -capitalized, -hour
+                      -NumberPositiveEmotions, -Positive, -Negative, - NumberNegativeEmotions )
+nb_mod6 <- naive_bayes(x=no_negative, y=train_labels )
+#Training Data
+pred_train_6 <- predict(nb_mod6, train)
+check_accuracy(pred_train_6, train_labels, "Training")
+#Test Data
+pred_test_6 <- predict(nb_mod6, test)
+check_accuracy(pred_test_6, test_labels, "Test")
+
+#Model 7: Drop Exclamation Point
+no_exclamation <- select(train, link, hashtag, quote)
+nb_mod7 <- naive_bayes(x=no_exclamation, y=train_labels )
+#Training Data
+pred_train_7 <- predict(nb_mod7, train)
+check_accuracy(pred_train_7, train_labels, "Training")
+#Test Data
+pred_test_7 <- predict(nb_mod7, test)
+check_accuracy(pred_test_7, test_labels, "Test")
+
+#Model 8: Drop Hashtag
+drop_hash <- select(train, quote,  link)
+nb_mod8 <- naive_bayes(x=drop_hash, y=train_labels )
+pred_train_8 <- predict(nb_mod8, train)
+check_accuracy(pred_train_8, train_labels, "Training")
+#Test Data
+pred_test_8 <- predict(nb_mod8, test)
+check_accuracy(pred_test_8, test_labels, "Test")
+'It looks like we should keep hashtag. '
+
+
+#Model 9: Drop Quote
+drop_quote <- select(train, quote, hashtag,  link)
+nb_mod9 <- naive_bayes(x=drop_quote, y=train_labels )
+pred_train_9 <- predict(nb_mod9, train)
+check_accuracy(pred_train_9, train_labels, "Training")
+#Test Data
+pred_test_9 <- predict(nb_mod9, test)
+check_accuracy(pred_test_9, test_labels, "Test")
+
+#Model  With Link and Quote
+nb_mod13 <- naive_bayes(x=select(train, link, quote), y=train_labels)
+pred_train_13 <- predict(nb_mod13, train)
+check_accuracy(pred_train_13, train_labels, "Training")
+#Test Data
+pred_test_13 <- predict(nb_mod11, test)
+check_accuracy(pred_test_13, test_labels, "Test")
+
+
+#Model with Hashtag and Quote
+nb_mod11 <- naive_bayes(x=select(train, hashtag,  quote), y=train_labels)
+pred_train_11 <- predict(nb_mod11, train)
+check_accuracy(pred_train_11, train_labels, "Training")
+#Test Data
+pred_test_11 <- predict(nb_mod11, test)
+check_accuracy(pred_test_11, test_labels, "Test")
+
+
+
+
+#Best Models
+
+
+#Best Models: 10 & 12
+
+#Model 10: Link and Hashtag
+nb_mod10 <- naive_bayes(x=select(train, link, hashtag), y=train_labels)
+pred_train_10 <- predict(nb_mod10, train)
+check_accuracy(pred_train_10, train_labels, "Training")
+#Test Data
+pred_test_10 <- predict(nb_mod10, test)
+check_accuracy(pred_test_10, test_labels, "Test")
+
+#Model 12: Link, Quote, and Hashtag 
+nb_mod12 <- naive_bayes(x=select(train, link, quote, hashtag), y=train_labels)
+pred_train_12 <- predict(nb_mod12, train)
+check_accuracy(pred_train_12, train_labels, "Training")
+#Test Data
+pred_test_12 <- predict(nb_mod12, test)
+check_accuracy(pred_test_12, test_labels, "Test")
+
 
 
 
